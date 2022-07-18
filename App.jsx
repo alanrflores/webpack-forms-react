5a2de628a4e8835fadb75c2c7f4e11218883f7d2
alanrflores <alan.flores640@gmail.com>
2022-07-17 23:34:16 -0300

    nuevos commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './app.css'
+
+const App = () => {
+  return (
+    <div className={styles.contenedor}>
+        <h1 className={styles.title}>Hola mundo como va?</h1>
+    </div>
+  )
+}
+
+export default App
